6d1ecc2356bb36f8c42a048252bbee508ea677bf
david-muwonge <falconmclug@gmail.com>
2021-10-09 18:19:38 +0300

    comment in database.sql

--- database.sql
+++ database.sql
@@ -1,42 +1,41 @@
-CREATE DATABASE perntodo;
-
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255), description1 VARCHAR(255),
-);
+--CREATE DATABASE perntodo;
 
 
+--ALTER TABLE accounts ALTER COLUMN description5 DROP NOT NULL;
+--ALTER TABLE accounts ALTER COLUMN description6 DROP NOT NULL;
+
+
+--TRUNCATE accounts RESTART IDENTITY;
+
+--ALTER TABLE accounts RENAME image TO images;
+
+--ALTER TABLE accounts ADD COLUMN images VARCHAR(255);
+
+--UPDATE accounts SET image = img WHERE user_id = 1;
+
+--units VARCHAR(255),
+--gasalert VARCHAR(255),
+--tamper VARCHAR(255),
+--battery VARCHAR(255),
+--latitude VARCHAR(255),
+--\c longitude VARCHAR(255)
 
 CREATE TABLE accounts (
-	user_id serial PRIMARY KEY,
-	description VARCHAR ( 255 )  NOT NULL,
-	description1 VARCHAR ( 255 ) NOT NULL,
-	description2 VARCHAR ( 255 ) UNIQUE NOT NULL,
-    description3 VARCHAR ( 255 ) NOT NULL,
-    description4 VARCHAR ( 255 ) NOT NULL,
-    description5 VARCHAR ( 255 ) UNIQUE NOT NULL,
-    description6 VARCHAR ( 255 ) UNIQUE NOT NULL
-	
-);
-
-ALTER TABLE accounts ALTER COLUMN description DROP NOT NULL;
-ALTER TABLE accounts ALTER COLUMN description3 DROP NOT NULL;
-ALTER TABLE accounts ALTER COLUMN description4 DROP NOT NULL;
-ALTER TABLE accounts ALTER COLUMN description5 DROP NOT NULL;
-ALTER TABLE accounts ALTER COLUMN description6 DROP NOT NULL;
-
-
-TRUNCATE accounts RESTART IDENTITY;
-
-ALTER TABLE accounts RENAME image TO images;
-
-ALTER TABLE accounts ADD COLUMN images VARCHAR(255);
-
-UPDATE accounts SET image = img WHERE user_id = 1;
-
+user_id serial PRIMARY KEY,
+firstname VARCHAR(255),
+lastname VARCHAR(255),
+mobile VARCHAR(255),
+location_detail VARCHAR(255),
+district VARCHAR(255),
+serial VARCHAR(255),
+nin VARCHAR(255),
+account_num VARCHAR(255),
+token VARCHAR(255),
 units VARCHAR(255),
 gasalert VARCHAR(255),
 tamper VARCHAR(255),
 battery VARCHAR(255),
 latitude VARCHAR(255),
 longitude VARCHAR(255)
+	
+);
