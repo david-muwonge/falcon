560124a0fdf958d2564142ad29f50814ea8f1897
david-muwonge <92211157+david-muwonge@users.noreply.github.com>
2021-10-16 07:37:24 +0300

    welcom

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
     return (<Fragment>
       <div className="">
-        <h1 className="container text-center text-warning  font-weight-bold">FALCON GAS</h1>
+        <h1 className="text-center text-warning  font-weight-bold">FALCON GAS</h1>
         <div>
           <div>
             <label className="container text-center text-success font-weight-bold">Please select account type:</label>
               <div>
               <ul Style = "list-style-type:none">
                 <li><Link to="/viewUsers">
-                    <button className="btn btn-warning text-success float-left font-weight-bold mb-2">Agent</button></Link>
+                    <button className="btn btn-warning col-md-10 text-success float-left font-weight-bold mb-2">Agent</button></Link>
                   </li>
                   <div>
                   <li><Link to="/">
