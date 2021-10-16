ca006867ada3fda7b7be4e22aaef570ccef6ba51
david-muwonge <92211157+david-muwonge@users.noreply.github.com>
2021-10-16 07:44:27 +0300

    welcom

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,15 +7,15 @@ const Welcome = () => {
         <h1 className="text-center text-warning  font-weight-bold">FALCON GAS</h1>
         <div>
           <div>
-            <label className="container text-center text-success font-weight-bold">Please select account type:</label>
+            <label className="text-center text-success font-weight-bold">Please select account type:</label>
               <div>
               <ul Style = "list-style-type:none">
                 <li><Link to="/viewUsers">
-                    <button className="btn btn-warning col-md-6 text-success float-left font-weight-bold mb-2">Agent</button></Link>
+                    <button className="btn btn-warning col-md-3 text-success float-left font-weight-bold mb-2">Agent</button></Link>
                   </li>
                   <div>
                   <li><Link to="/">
-                    <button class="btn btn-warning text-success font-weight-bold mb-2">Customer</button></Link>
+                    <button class="col-md-3 btn btn-warning text-success font-weight-bold mb-2">Customer</button></Link>
                   </li>                    
                   </div>
                 </ul>               
